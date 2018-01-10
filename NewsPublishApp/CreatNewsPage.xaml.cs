d95813e63077bb3eaf908ad6b2525ba364d65d03
УринбасаровД <@A-305-10>
2018-01-10 17:53:45 +0600

    first

---
+++ NewsPublishApp/CreatNewsPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsPublishApp
+{
+    /// <summary>
+    /// Логика взаимодействия для CreatNewsPage.xaml
+    /// </summary>
+    public partial class CreatNewsPage : Page
+    {
+        public CreatNewsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.ShowDialog();
+
+            string filePath = dialog.FileName;
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filePath);
+            image.EndInit();
+
+            imageConteiner.Source = image;
+        }
+    }
+}
---
+++ NewsPublishApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsPublishApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Page.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private MainWindow mainWindow;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public MainPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void creatNewsButtonClick(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Content = new CreatNewsPage();
+        }
+    }
+}
